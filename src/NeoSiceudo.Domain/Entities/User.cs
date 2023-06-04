3e1702f60021d3ca05dc37a4ebd19c0dbb645d51
Gustavo Silva <gustavoasc2004@gmail.com>
2023-06-03 20:02:59 -0400

    Added shared folder with needed classes.

---
+++ src/NeoSiceudo.Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeoSiceudo.Domain.Primitives;
+
+namespace NeoSiceudo.Domain.Entities;
+
+public sealed class User : AggregateRoot, IAuditableEntity
+{
+	public User(Guid id) : base(id)
+	{
+		
+	}
+
+	public Name Firstname { get; private set; }
+	public Name	Lastname { get; private set; }
+	public DateTime CreatedOnUtc { get; init; }
+	public DateTime? LastModifiedUtc { get; set; }
+}
---
+++ src/NeoSiceudo.Domain/Errors/DomainErrors.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeoSiceudo.Domain.Shared;
+
+namespace NeoSiceudo.Domain.Errors;
+
+public static class DomainErrors
+{
+	public static class Name
+	{
+		public static readonly Error Empty = new(
+			"Name.Empty",
+			"Name contains a null or empty value");
+
+		public static readonly Error TooLong = new(
+			"Name.TooLong",
+			"Name exceeded the maximum characters capacity.");
+	}
+}
