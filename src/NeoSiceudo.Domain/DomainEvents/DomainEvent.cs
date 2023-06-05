b9e4925b382d0ae7f50b9bea2d73cbaa9bbd3f6f
Gustavo Silva <gustavoasc2004@gmail.com>
2023-06-04 22:38:58 -0400

    Added DomainEvent

---
+++ src/NeoSiceudo.Domain/DomainEvents/DomainEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeoSiceudo.Domain.Primitives;
+
+namespace NeoSiceudo.Domain.DomainEvents;
+
+public abstract record DomainEvent(Guid Id) : IDomainEvent;
