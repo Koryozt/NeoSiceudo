aa8f11cccd114efe33924fda24279407c7f1d5e8
Gustavo Silva <gustavoasc2004@gmail.com>
2023-06-04 22:07:46 -0400

    Modified shared classes.

--- src/NeoSiceudo.Domain/Shared/Result.cs
+++ src/NeoSiceudo.Domain/Shared/Result.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NeoSiceudo.Domain.Shared;
+﻿namespace NeoSiceudo.Domain.Shared;
 
 public class Result
 {
