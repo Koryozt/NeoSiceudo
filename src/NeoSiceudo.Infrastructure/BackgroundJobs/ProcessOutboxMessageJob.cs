679f6db3ef61813a7ffd45bac1fbf0b05d7af3c1
Gustavo Silva <gustavoasc2004@gmail.com>
2023-06-22 16:08:38 -0400

    Created Infrastructure Layer,

---
+++ src/NeoSiceudo.Infrastructure/BackgroundJobs/ProcessOutboxMessageJob.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NeoSiceudo.Domain.Primitives;
+using NeoSiceudo.Persistence;
+using NeoSiceudo.Persistence.Outbox;
+using Newtonsoft.Json;
+using Quartz;
+
+namespace NeoSiceudo.Infrastructure.BackgroundJobs;
+
+[DisallowConcurrentExecution]
+public class ProcessOutboxMessagesJob : IJob
+{
+	private readonly ApplicationDbContext _dbContext;
+	private readonly IPublisher _publisher;
+
+	public ProcessOutboxMessagesJob(ApplicationDbContext dbContext, IPublisher publisher)
+	{
+		_dbContext = dbContext;
+		_publisher = publisher;
+	}
+
+	public async Task Execute(IJobExecutionContext context)
+	{
+		List<OutboxMessage> messages = await _dbContext
+			.Set<OutboxMessage>()
+			.Where(m => m.ProcessedOnUtc == null)
+			.Take(20)
+			.ToListAsync(context.CancellationToken);
+
+		foreach (OutboxMessage outboxMessage in messages)
+		{
+			IDomainEvent? domainEvent = JsonConvert
+				.DeserializeObject<IDomainEvent>(
+					outboxMessage.Content,
+					new JsonSerializerSettings
+					{
+						TypeNameHandling = TypeNameHandling.All
+					});
+
+			if (domainEvent is null)
+			{
+				continue;
+			}
+
+			await _publisher.Publish(domainEvent, context.CancellationToken);
+
+			outboxMessage.ProcessedOnUtc = DateTime.UtcNow;
+		}
+
+		await _dbContext.SaveChangesAsync();
+	}
+}
