3d2e5fb361a2bff7bd8b4ce4d469f2221d6b3c0a
Gustavo Silva <gustavoasc2004@gmail.com>
2023-06-27 17:46:41 -0400

    Improved code readability.

--- src/NeoSiceudo.Infrastructure/Authentication/HasPermissionAttribute.cs
+++ src/NeoSiceudo.Infrastructure/Authentication/HasPermissionAttribute.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using NeoSiceudo.Domain.Enumerators;
 
 namespace NeoSiceudo.Infrastructure.Authentication;
